9d7e3a47919b6d71af7887c25ad6d42516eb2caa
Brendan <me@brendanpeterson.com>
2022-03-26 00:03:57 -0700

    WebView now queries database for Current Temps

--- WebView.py
+++ WebView.py
@@ -1,14 +1,47 @@
 #!/usr/bin/python3
-#from art import *
-#Art= text2art("TEST", font='block',chr_ignore=True)
-#print('Content-Type: text/plain')
-#print('')
-#print('This is my test')
-#print(Art)
+
+import os
+import sqlite3
+import datetime
+
+db_dir = "/home/pi/GreenHouseLogs"
+connection = sqlite3.connect(db_dir+'/Greenhouse_data.db')
+
+
+
 print ("Content-Type: text/html")
 print ('') 
 print ("""
-      <TITLE>CGIBREN</TITLE>
-      <H1> This is my first SCRIPT </H1>
-      moretextforue
+      <TITLE>CoonCreek Green House</TITLE>
+      <H2> Greenhouse Temperatures</H2>
+
+
       """)
+
+today = datetime.date.today()
+#cursor = connection.execute("select distinct(therm_name) from temps order by therm_name")
+#for row in cursor.fetchall():
+    #print(row[0])
+#print('')
+#print("<TABLE BORDER = 1><TR><TD></TD><TD>CURRENT</TD><TD bgcolor = 'pink'>24h High</TD><TD bgcolor = 'lightblue'>24h Low</TD></TR>")
+print("<TABLE BORDER = 1><TR><TD></TD><TD>CURRENT</TD></TR>")
+
+cursor = connection.execute("select therm_name,ftemp,max(datetime) from temps group by therm_name order by therm_name;")
+for row in cursor.fetchall():
+    print("<TR><TD>%s</TD><TD>%2.1f</TD></TR>" %(row[0],row[1]) )
+cursor.close()
+print("</TABLE>")
+   
+#cursor = connection.execute("select distinct(therm_name) from temps order by therm_name")
+#therm_list = cursor.fetchall()
+#cursor.close()
+
+#for row in therm_list:
+    ##lowcursor = connection.execute("select therm_name,max(ftemp),datetime from temps where threm_name = '"+row[0]+"' and datetime > "
+    #print lowcursor.fetchall()
+
+
+
+
+
+
