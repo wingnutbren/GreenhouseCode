01e1f2fb77d9a0eff88d16a04d23db157ade8ff6
Brendan <me@brendanpeterson.com>
2022-03-26 16:31:50 -0700

    Added icon and modified WebView

--- WebView.py
+++ WebView.py
@@ -13,9 +13,6 @@ print ("Content-Type: text/html")
 print ('') 
 print ("""
       <TITLE>CoonCreek Green House</TITLE>
-      <H2> Greenhouse Temperatures</H2>
-
-
       """)
 
 today = datetime.date.today()
@@ -23,12 +20,12 @@ today = datetime.date.today()
 #for row in cursor.fetchall():
     #print(row[0])
 #print('')
-#print("<TABLE BORDER = 1><TR><TD></TD><TD>CURRENT</TD><TD bgcolor = 'pink'>24h High</TD><TD bgcolor = 'lightblue'>24h Low</TD></TR>")
-print("<TABLE BORDER = 1><TR><TD></TD><TD>CURRENT</TD></TR>")
+#print("<TABLE BORDER = 0><TR><TD></TD><TD>CURRENT</TD><TD bgcolor = 'pink'>24h High</TD><TD bgcolor = 'lightblue'>24h Low</TD></TR>")
+print("<TABLE BORDER = 0><TR><TD></TD><TD>CURRENT</TD></TR>")
 
 cursor = connection.execute("select therm_name,ftemp,max(datetime) from temps group by therm_name order by therm_name;")
 for row in cursor.fetchall():
-    print("<TR><TD>%s</TD><TD>%2.1f</TD></TR>" %(row[0],row[1]) )
+    print("<TR><TD padx='10'>%s</TD><TD>%2.1f</TD></TR>" %(row[0],row[1]) )
 cursor.close()
 print("</TABLE>")
    
