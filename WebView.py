3f2b587a3d8a4891ea7b46d3694d09ba29396e61
Brendan <me@brendanpeterson.com>
2022-03-26 18:30:56 -0700

    Added Today's Highs and Lows

--- WebView.py
+++ WebView.py
@@ -11,31 +11,43 @@ connection = sqlite3.connect(db_dir+'/Greenhouse_data.db')
 
 print ("Content-Type: text/html")
 print ('') 
-print ("""
-      <TITLE>CoonCreek Green House</TITLE>
-      """)
+print (" <TITLE>CoonCreek Green House</TITLE>")
 
 today = datetime.date.today()
-#cursor = connection.execute("select distinct(therm_name) from temps order by therm_name")
-#for row in cursor.fetchall():
-    #print(row[0])
-#print('')
-#print("<TABLE BORDER = 0><TR><TD></TD><TD>CURRENT</TD><TD bgcolor = 'pink'>24h High</TD><TD bgcolor = 'lightblue'>24h Low</TD></TR>")
-print("<TABLE BORDER = 0><TR><TD></TD><TD>CURRENT</TD></TR>")
-
-cursor = connection.execute("select therm_name,ftemp,max(datetime) from temps group by therm_name order by therm_name;")
+print("<TABLE BORDER = 0 PADX='10px'><TR><TD>CURRENT</TD><TD></TD></TR>")
+therm_list = []
+cursor = connection.execute("select therm_name,ftemp,strftime('%H:%M',max(datetime)) from temps group by therm_name order by therm_name;")
 for row in cursor.fetchall():
-    print("<TR><TD padx='10'>%s</TD><TD>%2.1f</TD></TR>" %(row[0],row[1]) )
+    therm_list.append(row[0])
+    print("<TR bgcolor = 'lightgray'><TD>%s</TD><TD>%2.1f&#176;</TD><TD>@ %s</TD></TR>" %(row[0],row[1],row[2]) )
 cursor.close()
 print("</TABLE>")
-   
-#cursor = connection.execute("select distinct(therm_name) from temps order by therm_name")
-#therm_list = cursor.fetchall()
-#cursor.close()
 
-#for row in therm_list:
-    ##lowcursor = connection.execute("select therm_name,max(ftemp),datetime from temps where threm_name = '"+row[0]+"' and datetime > "
-    #print lowcursor.fetchall()
+
+   
+#Print the Daily LOWS table
+print("<HR>")
+print("<TABLE><TR><TD>Todays Lows</TD><TD></TD></TR>")
+for therm_name in therm_list:
+    query = "select min(ftemp),strftime('%%H:%%M',datetime)as time from temps where therm_name = '%s' and datetime > '%s' " % (therm_name,today)
+    lowcursor = connection.cursor();
+    lowcursor.execute(query)
+
+    for lowrow in lowcursor.fetchall():
+        print ("<TR bgcolor = 'lightblue'><TD>%s</TD><TD>%2.1f&#176;</TD><TD>@ %s</TD></TR>"% (therm_name,lowrow[0],lowrow[1] )) 
+print("</TABLE>")
+
+#Print the Daily HIGHS table
+print("<HR>")
+print("<TABLE><TR><TD>Todays Highs</TD><TD></TD></TR>")
+for therm_name in therm_list:
+    query = "select max(ftemp),strftime('%%H:%%M',datetime)as time from temps where therm_name = '%s' and datetime > '%s' " % (therm_name,today)
+    highcursor = connection.cursor();
+    highcursor.execute(query)
+
+    for highrow in highcursor.fetchall():
+        print ("<TR bgcolor = 'pink'><TD>%s</TD><TD>%2.1f&#176;</TD><TD>@ %s</TD></TR>"% (therm_name,highrow[0],highrow[1] )) 
+print("</TABLE>")
 
 
 
